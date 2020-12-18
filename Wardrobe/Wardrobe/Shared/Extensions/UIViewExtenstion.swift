1d9fdf0fefc3ee118891dc5826692fb9d578ec5a
Erokhin Nikita <coffeemilk1337@gmail.com>
2020-12-18 18:35:45 +0300

    Added shared stuff

---
+++ Wardrobe/Wardrobe/Shared/Extensions/UIViewExtenstion.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension UIView {
+    func dropShadow(offset: CGSize = CGSize(width: 1, height: 3), radius: CGFloat = 4, opacity: Float = 0.4) {
+        self.layer.shadowOffset = offset
+        self.layer.shadowRadius = radius
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = opacity
+    }
+}
