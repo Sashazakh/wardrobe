bcda413da7f6b63d87c97db770d9d702aa84fb1d
Vlad <vkrivozubov@list.ru>
2021-01-10 02:52:12 +0300

    [FIX] login & register input

--- Wardrobe/Wardrobe/Register/Presenter/RegisterPresenter.swift
+++ Wardrobe/Wardrobe/Register/Presenter/RegisterPresenter.swift
@@ -46,15 +46,33 @@ extension RegisterPresenter: RegisterViewOutput {
             return
         }
 
+        guard login.isValidString() else {
+            let msg = "Возможны только строчные и заглавные буквы латинского алфавита (a-z, A-Z) и цифры от 0 до 9"
+            self.showAlert(title: "Недопустимые символы в поле ввода пароля!", message: msg)
+            return
+        }
+
         guard let password = (userCredentials["password"] ?? ""),
               !password.isEmpty else {
             view?.showAlert(title: "Ошибка", message: "Введите пароль")
             return
         }
 
+        guard password.isValidString() else {
+            let msg = "Возможны только строчные и заглавные буквы латинского алфавита (a-z, A-Z) и цифры от 0 до 9"
+            self.showAlert(title: "Недопустимые символы в поле ввода пароля!", message: msg)
+            return
+        }
+
         guard let repeatPassword = (userCredentials["repeatPassword"] ?? ""),
               !repeatPassword.isEmpty else {
             view?.showAlert(title: "Ошибка", message: "Повторите пароль")
+            return
+        }
+
+        guard repeatPassword.isValidString() else {
+            let msg = "Возможны только строчные и заглавные буквы латинского алфавита (a-z, A-Z) и цифры от 0 до 9"
+            self.showAlert(title: "Недопустимые символы в поле ввода пароля!", message: msg)
             return
         }
 
